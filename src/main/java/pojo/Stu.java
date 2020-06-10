5f1250f37fb8f8621557c280f16b2698852d6315
kaiye <1474914223@qq.com>
2020-06-10 11:53:40 +0800

    第一次提交项目

---
+++ src/main/java/pojo/Stu.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Stu {
+    private int sid;
+    private String sname;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+}
